9421cdeb8d7837a52454ea7130606a66df006880
Femke <47941887+FemkePriemis@users.noreply.github.com>
2023-05-13 15:06:22 +0200

    Delete PuppyPlatform directory

--- PuppyPlatform/PuppyPlatform/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace PuppyPlatform.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
